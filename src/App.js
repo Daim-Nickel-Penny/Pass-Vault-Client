57e62f72dee1dd89138bf6a1b5bddfd146ac6e6e
1804449@kiit.ac.in <1804449@kiit.ac.in>
2021-04-24 21:14:12 +0530

    Connected With Backend. Styling Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [passwordList, setPasswordList] = useState([]);
+
+  const addPassword = () => {
+    axios.post("http://localhost:3001/addpassword", {
+      password: password,
+      title: title,
+    });
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/showpasswords").then((response) => {
+      console.log(response.data);
+      setPasswordList(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AddingPassword">
+        <input
+          type="text"
+          placeholder="password@123"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          placeholder="Instagram"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+        <button onClick={addPassword}>Add Password</button>
+      </div>
+
+      <div className="Passwords">
+        {passwordList.map((val) => {
+          return (
+            <div className="password">
+              <h3>{val.title}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
